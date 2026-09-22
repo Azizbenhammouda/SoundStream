12bad75424a187e40872821adf404c77bc6d1322
Aziz Ben Hammouda <azizbhammouda@gmail.com>
2026-09-22 21:24:29 +0100

    Create and finding users in DB

--- users/model.go
+++ users/model.go
@@ -1,14 +1,18 @@
 package users
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type User struct {
-	ID                int       `json:"id" gorm:"primaryKey"`
-	Username          string    `json:"username" gorm:"unique;not null"`
-	Email             string    `json:"email" gorm:"unique;not null"`
-	Password_hash     string    `json:"-"`
-	Display_name      string    `json:"display_name"`
-	Profile_image_url *string   `json:"profile_image_url,omitempty" gorm:"column:profile_image_url"`
-	Created_at        time.Time `json:"created_at"`
-	Updated_at        time.Time `json:"updated_at"`
+	ID              uuid.UUID `json:"id" gorm:"type:uuid;primaryKey;default:gen_random_uuid()"`
+	Username        string    `json:"username" gorm:"unique;not null"`
+	Email           string    `json:"email" gorm:"unique;not null"`
+	PasswordHash    string    `json:"-"`
+	DisplayName     string    `json:"display_name"`
+	ProfileImageURL *string   `json:"profile_image_url,omitempty" gorm:"column:profile_image_url"`
+	CreatedAt       time.Time `json:"created_at"`
+	UpdatedAt       time.Time `json:"updated_at"`
 }
--- users/repository.go
+++ users/repository.go
@@ -1,15 +1,54 @@
 package users
 
 import (
-	"gorm.io/driver/postgres"
+	"github.com/google/uuid"
+
 	"gorm.io/gorm"
 )
 
-func ConnectDB() (*gorm.DB, error) {
-	dsn := "host=localhost user=postgres password=yourpassword dbname=mydb port=5432 sslmode=disable"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		return nil, err
+type UserRepository interface {
+	Create(user *User) error
+	GetByID(id uuid.UUID) (*User, error)
+	GetByEmail(email string) (*User, error)
+	Update(user *User) error
+	Delete(id uuid.UUID) error
+}
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return userRepository{
+		db: db,
 	}
-	return db, nil
+}
+func (u userRepository) Create(user *User) error {
+	result := u.db.Create(user)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+func (u userRepository) GetByID(id uuid.UUID) (*User, error) {
+	var user User
+	result := u.db.Where("id = ?", id).First(&user)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &user, nil
+}
+func (u userRepository) GetByEmail(email string) (*User, error) {
+	var user User
+	result := u.db.Where("email = ?", email).First(&user)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &user, nil
+}
+func (u userRepository) Update(user *User) error {
+
+	return nil
+}
+func (u userRepository) Delete(id uuid.UUID) error {
+	return nil
 }
