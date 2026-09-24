90e896333dd79d92fe905a5023c8f0f20b54c21f
Aziz Ben Hammouda <azizbhammouda@gmail.com>
2026-09-25 00:25:39 +0100

    Start of user handler

--- users/handler.go
+++ users/handler.go
@@ -1,1 +1,25 @@
 package users
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type userHandler struct {
+	service UserService
+}
+
+func NewUserHandler(s UserService) userHandler {
+	return userHandler{
+		service: s,
+	}
+}
+func (h userHandler) Register(w http.ResponseWriter, req *http.Request) {
+	var input RegisterInput
+	err := json.NewDecoder(req.Body).Decode(&input)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+}
